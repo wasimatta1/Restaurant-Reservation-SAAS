f01bd209b447a6693c1695ac3dc38b0ae71d95b1
wasim atta <wasim.atta33@hotmail.com>
2024-10-08 22:34:09 +0300

    Create RestaurantReservationDbContext With Connection string to DB

---
+++ RestaurantReservation.Db/Data/RestaurantReservationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace RestaurantReservation.Db.Data
+{
+    public class RestaurantReservationDbContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetSection("constr").Value;
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(RestaurantReservationDbContext).Assembly);
+        }
+    }
+}
